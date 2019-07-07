4d513241470c478e1e0ecc82c075d8add5c08090
Taffy Brecknock <brecknockt@gmail.com>
2019-07-07 21:46:52 +0100

    webpack config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import _ from 'lodash'
+import './style.css';
 
+import layout from './images/layout.png';
+
+var layoutImg = document.getElementById('layout');
+layoutImg.src = layout;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode:'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+	filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+	rules: [
+		{
+		  test: /\.css$/,
+		  use: [
+			  'style-loader',
+			  'css-loader'
+		  ]
+	  	}/*,
+		{
+			test: /\.(png|svg|jpg|gif)$/,  
+            use: [{
+                loader: 'url-loader',
+                options: { 
+                    limit: 8000, // Convert images < 8kb to base64 strings
+                    name: 'images/[name].[ext]'
+                } 
+            }]
+		}*/
+	  ]
+	},
+	"plugins":[ 
+		new CopyWebpackPlugin([
+            {from:'src/images',to:'images'} ,
+            {from:'src/index.html',to:'index.html'}
+        ])
+	  ]
 };
