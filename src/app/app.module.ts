02e4f9b95ce33b71ec48191ba33ffb12b9a14ec9
Taffy Brecknock <brecknockt@gmail.com>
2019-07-21 12:31:01 +0100

    add DealerService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Play_areaComponent } from './play_area/play_area.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Play_areaComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
